b753ceeee93167354f0556a16728ae83bef44202
blackshirt <blackshirtmuslim@yahoo.com>
2023-07-28 22:00:57 +0700

    fix test and remove v.mod mode

--- hkdf.v
+++ hkdf.v
@@ -1,23 +1,214 @@
 module hkdf
 
+import math
 import crypto
 import crypto.hmac
 import crypto.sha1
 import crypto.sha256
 import crypto.sha512
 import encoding.binary
+import buffer
 
 // Hasher is hashing function in standard library.
 // see https://modules.vlang.io/crypto.html#Hash
 // but for this purpose, its limited to sha based hash.
+//
 pub type Hasher = crypto.Hash
 
-fn (h Hasher) hmac_new(key []u8, data []u8) ![]u8 {
+// HMAC based Key Derivation Function
+pub struct Hkdf {
+	hasher Hasher = .sha256
+}
+
+pub fn new(h Hasher) &Hkdf {
+	return &Hkdf{h}
+}
+
+pub fn (h Hkdf) hasher() Hasher {
+	return h.hasher
+}
+
+// hmac create new hmac
+pub fn (h Hkdf) hmac(key []u8, data []u8) ![]u8 {
+	return h.hasher.create_hmac(key, data)!
+}
+
+pub fn (h Hkdf) extract(salt []u8, ikm []u8) ![]u8 {
+	mut inp := ikm.clone()
+	if inp.len == 0 {
+		// is it should to init to hash.len null arrays instead
+		inp = []u8{len: h.hasher.size()!, init: u8(0x00)}
+	}
+
+	mut slt := salt.clone()
+	if slt.len == 0 {
+		slt = []u8{len: h.hasher.size()!, init: u8(0x00)}
+	}
+
+	prk := h.hmac(slt, inp)!
+	return prk
+}
+
+// L is the length of output keying material in octets (<= 255*HashLen)
+// where HashLen denotes the length of the hash function output in octets
+pub fn (h Hkdf) expand(prk []u8, info []u8, length int) ![]u8 {
+	hash_len := h.hasher.size()!
+
+	if length > 255 * hash_len {
+		return error('Cannot expand to more than 255 * ${hash_len}')
+	}
+	ceil := if length % hash_len == 0 { 0 } else { 1 }
+	blk := length / hash_len + ceil
+	mut okm := []u8{}
+	mut ob := []u8{}
+	for i := 0; i < blk; i++ {
+		ob << info
+		ctr := i + 1
+		ob << [u8(ctr)]
+		ob = h.hmac(prk, ob)!
+
+		okm << ob
+	}
+	return okm[..length]
+}
+
+// Support for HKDF-Expand-Label and other machinery for TLS 1.3
+// from RFC8446 Section 7.1 Key Schedule and others.
+// see https://datatracker.ietf.org/doc/html/rfc8446#section-7.1
+
+// HKDF-Expand-Label(Secret, Label, Context, Length) =
+//      HKDF-Expand(Secret, HkdfLabel, Length)
+pub fn (h Hkdf) expand_label(secret []u8, label string, context []u8, length int) ![]u8 {
+	lbl := new_hkdf_label(label, context, length)!
+	hkdflabel := lbl.encode()!
+	out := h.expand(secret, hkdflabel, length)!
+	return out
+}
+
+// Derive-Secret(Secret, Label, Messages) =
+//     HKDF-Expand-Label(Secret, Label,  Transcript-Hash(Messages), Hash.length)
+pub fn (h Hkdf) derive_secret(secret []u8, label string, messages []u8) ![]u8 {
+	trc_hash := h.hasher.sum(messages)!
+	out := h.expand_label(secret, label, trc_hash, h.hasher.size()!)!
+	return out
+}
+
+// struct {
+//      uint16 length = Length;
+//      opaque label<7..255> = 'tls13 ' + Label;
+//      opaque context<0..255> = Context;
+// } HkdfLabel;
+//
+// With common hash functions, any label longer than 12 characters
+// requires an additional iteration of the hash function to compute.
+// The labels in this specification have all been chosen to fit within
+// this limit.
+
+const (
+	max_hkdf_label_length   = 255
+	max_hkdf_context_length = 255
+)
+
+struct HkdfLabel {
+	length  int // u16
+	prefix  string = 'tls13 '
+	label   string // ascii string
+	context []u8   // < 255 len
+}
+
+fn (hl HkdfLabel) verify() ! {
+	if !hl.label.is_ascii() {
+		return error('HkdfLabel.label contains non-ascii string')
+	}
+	if hl.context.len > hkdf.max_hkdf_context_length {
+		return error('hkdf.context.len exceed limit')
+	}
+	label := hl.prefix + hl.label
+	if label.len > hkdf.max_hkdf_label_length {
+		return error('label.len exceed limit')
+	}
+	if hl.length > math.max_u16 {
+		return error('hl.length exceed limit')
+	}
+}
+
+fn new_hkdf_label(label string, context []u8, length int) !HkdfLabel {
+	hl := HkdfLabel{
+		length: length
+		label: label
+		context: context
+	}
+	hl.verify()!
+	return hl
+}
+
+fn (hl HkdfLabel) encode() ![]u8 {
+	hl.verify()!
+	mut out := []u8{}
+	label := hl.prefix + hl.label
+
+	// writes hkdf length
+	mut ln := []u8{len: 2}
+	binary.big_endian_put_u16(mut ln, u16(hl.length))
+	out << ln
+
+	// writes label length
+	label_length := label.bytes().len // should fit in one byte
+	out << u8(label_length)
+	out << label.bytes()
+
+	out << u8(hl.context.len)
+	out << hl.context
+
+	return out
+}
+
+fn HkdfLabel.decode(b []u8) !HkdfLabel {
+	mut r := buffer.new_reader(b)
+	// read two bytes length
+	length := r.read_u16()!
+	// one byte label length
+	label_len := r.read_byte()!
+	// read label contents
+	label := r.read_at_least(int(label_len))!
+	// one byte context len
+	ctx_len := r.read_byte()!
+	// read context bytes
+	ctx := r.read_at_least(int(ctx_len))!
+
+	hklabel := new_hkdf_label(label.str(), ctx, int(length))!
+	return hklabel
+}
+
+// Hasher
+pub fn (h Hasher) sum(data []u8) ![]u8 {
+	match h {
+		.sha256 {
+			return sha256.sum256(data)
+		}
+		.sha512 {
+			return sha512.sum512(data)
+		}
+		else {
+			return error('unsupported sum hasher')
+		}
+	}
+}
+
+fn (h Hasher) create_hmac(key []u8, data []u8) ![]u8 {
 	match h {
 		.sha1 {
-			blksize := sha1.block_size
-			res := hmac.new(key, data, sha1.sum, blksize)
-			return res
+			// .sha1 is considerd as deprecated, so placed it in $if block,
+			// used only for testing and debug purposes.
+			// run with `$v -cg -stats test the_test.v`
+			$if test {
+				$if debug {
+					blksize := sha1.block_size
+					res := hmac.new(key, data, sha1.sum, blksize)
+					return res
+				}
+			}
+			return error('run it in test and debug mode')
 		}
 		.sha256 {
 			blksize := sha256.block_size
@@ -35,7 +226,7 @@ fn (h Hasher) hmac_new(key []u8, data []u8) ![]u8 {
 	}
 }
 
-fn (h Hasher) size() !int {
+pub fn (h Hasher) size() !int {
 	match h {
 		.sha1 {
 			return sha1.size
@@ -50,111 +241,4 @@ fn (h Hasher) size() !int {
 			return error('unsupported hash')
 		}
 	}
-}
-
-fn hmac_new(key []u8, data []u8, hashfn Hasher) ![]u8 {
-	return hashfn.hmac_new(key, data)!
-}
-
-pub fn extract(slt []u8, ikm []u8, hashfn Hasher) ![]u8 {
-	if ikm.len == 0 {
-		return error('bad ikm')
-	}
-
-	mut salt := slt.clone()
-	if salt.len == 0 {
-		salt = []u8{len: hashfn.size()!, init: u8(0x00)}
-	}
-
-	prk := hmac_new(salt, ikm, hashfn)!
-	return prk
-}
-
-// L is the length of output keying material in octets (<= 255*HashLen)
-// where HashLen denotes the length of the hash function output in octets
-pub fn expand(prk []u8, info []u8, length int, hashfn Hasher) ![]u8 {
-	hash_len := hashfn.size()!
-
-	if length > 255 * hash_len {
-		return error('Cannot expand to more than 255 * ${hash_len}')
-	}
-	ceil := if length % hash_len == 0 { 0 } else { 1 }
-	blk := length / hash_len + ceil
-	mut okm := []u8{}
-	mut ob := []u8{}
-	for i := 0; i < blk; i++ {
-		ob << info
-		ctr := i + 1
-		ob << [u8(ctr)]
-		ob = hmac_new(prk, ob, hashfn)!
-
-		okm << ob
-	}
-	return okm[..length]
-}
-
-pub fn hkdf(salt []u8, ikm []u8, info []u8, length int, hashfn Hasher) ![]u8 {
-	// Key derivation function
-	prk := extract(salt, ikm, hashfn)!
-	return expand(prk, info, length, hashfn)
-}
-
-// RFC8446 7.1.  Key Schedule
-// https://datatracker.ietf.org/doc/html/rfc8446#section-7.1
-//
-// HKDF-Expand-Label(Secret, Label, Context, Length) =
-//      HKDF-Expand(Secret, HkdfLabel, Length)
-//
-
-// Transcript-Hash(M1, M2, ... Mn) = Hash(M1 || M2 || ... || Mn)
-
-fn hkdf_expand_label(secret []u8, label string, context []u8, length int, hashfn Hasher) ![]u8 {
-	outlabel := 'tls13 ' + label
-	hl := HKDFLabel{
-		length: length
-		label: outlabel
-		context: context
-	}
-	info := hl.encode()!
-	res := expand(secret, info, length, hashfn)!
-	return res
-}
-
-// struct {
-//   	uint16 length = Length;
-//     	opaque label<7..255> = "tls13 " + Label;
-//   	opaque context<0..255> = Context;
-// } HkdfLabel;
-
-struct HKDFLabel {
-	length  int    // u16
-	label   string // ascii string
-	context []u8   // < 255 len
-}
-
-fn (hl HKDFLabel) encode() ![]u8 {
-	mut out := []u8{}
-	mut l := []u8{len: 2}
-	binary.big_endian_put_u16(mut l, u16(hl.length))
-	out << l
-
-	label_length := hl.label.bytes().len // fit in one byte
-	out << u8(label_length)
-	out << hl.label.bytes()
-
-	out << u8(hl.context.len)
-	out << hl.context
-
-	return out
-}
-
-// Derive-Secret(Secret, Label, Messages) =
-//     HKDF-Expand-Label(Secret, Label,  Transcript-Hash(Messages), Hash.length)
-//
-fn hkdf_derive_secret(secret []u8, label string, msg []u8, hashfn Hasher) ![]u8 {
-	context := hashfn.hmac_new(secret, msg)!
-	length := context.len
-
-	res := hkdf_expand_label(secret, label, context, length, hashfn)!
-	return res
 }
--- hkdf_test.v
+++ hkdf_test.v
@@ -25,12 +25,12 @@ fn test_hkdf_extract_expand_case_1() ! {
 	//      2d2d0a90cf1a5a4c5db02d56ecc4c5bf
 	//    34007208d5b887185865 (42 octets)
 
-	hasher := crypto.Hash.sha256
-	prkout := extract(salt, ikm, hasher)!
+	h := new(crypto.Hash.sha256)
+	prkout := h.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := h.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -77,12 +77,12 @@ fn test_hkdf_extract_expand_case_2() ! {
 	prk := hex.decode('06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244')!
 	okm := hex.decode('b11e398dc80327a1c8e7f78c596a49344f012eda2d4efad8a050cc4c19afa97c59045a99cac7827271cb41c65e590e09da3275600c2f09b8367793a9aca3db71cc30c58179ec3e87c14c01d5c1f3434f1d87')!
 
-	hasher := crypto.Hash.sha256
-	prkout := extract(salt, ikm, hasher)!
+	h := new(crypto.Hash.sha256)
+	prkout := h.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := h.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -114,12 +114,12 @@ fn test_hkdf_extract_expand_case_3() ! {
 	prk := hex.decode('19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04')!
 	okm := hex.decode('8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d9d201395faa4b61a96c8')!
 
-	hasher := crypto.Hash.sha256
-	prkout := extract(salt, ikm, hasher)!
+	hasher := new(crypto.Hash.sha256)
+	prkout := hasher.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := hasher.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -150,12 +150,12 @@ fn test_hkdf_extract_expand_case_4() ! {
 	prk := hex.decode('9b6c18c432a7bf8f0e71c8eb88f4b30baa2ba243')!
 	okm := hex.decode('085a01ea1b10f36933068b56efa5ad81a4f14b822f5b091568a9cdd4f155fda2c22e422478d305f3f896')!
 
-	hasher := crypto.Hash.sha1
-	prkout := extract(salt, ikm, hasher)!
+	hasher := new(crypto.Hash.sha1)
+	prkout := hasher.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := hasher.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -201,12 +201,12 @@ fn test_hkdf_extract_expand_case_5() ! {
 	prk := hex.decode('0x8adae09a2a307059478d309b26c4115a224cfaf6')!
 	okm := hex.decode('0x0bd770a74d1160f7c9f12cd5912a06ebff6adcae899d92191fe4305673ba2ffe8fa3f1a4e5ad79f3f334b3b202b2173c486ea37ce3d397ed034c7f9dfeb15c5e927336d0441f4c4300e2cff0d0900b52d3b4')!
 
-	hasher := crypto.Hash.sha1
-	prkout := extract(salt, ikm, hasher)!
+	hasher := new(crypto.Hash.sha1)
+	prkout := hasher.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := hasher.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -237,12 +237,12 @@ fn test_hkdf_extract_expand_case_6() ! {
 	prk := hex.decode('da8c8a73c7fa77288ec6f5e7c297786aa0d32d01')!
 	okm := hex.decode('0ac1af7002b3d761d1e55298da9d0506b9ae52057220a306e07b6b87e8df21d0ea00033de03984d34918')!
 
-	hasher := crypto.Hash.sha1
-	prkout := extract(salt, ikm, hasher)!
+	hasher := new(crypto.Hash.sha1)
+	prkout := hasher.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := hasher.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -274,12 +274,12 @@ fn test_hkdf_extract_expand_case_7() ! {
 	prk := hex.decode('2adccada18779e7c2077ad2eb19d3f3e731385dd')!
 	okm := hex.decode('2c91117204d745f3500d636a62f64f0ab3bae548aa53d423b0d1f27ebba6f5e5673a081d70cce7acfc48')!
 
-	hasher := crypto.Hash.sha1
-	prkout := extract(salt, ikm, hasher)!
+	hasher := new(crypto.Hash.sha1)
+	prkout := hasher.extract(salt, ikm)!
 
 	assert prk == prkout
 
-	okmout := expand(prkout, info, l, hasher)!
+	okmout := hasher.expand(prkout, info, l)!
 	assert okm == okmout
 }
 
@@ -331,9 +331,9 @@ fn test_hmac_new_sha256() {
 	]
 	// mut result := ''
 	for i, key in hkdf.keys {
-		hashfn := crypto.Hash.sha256
+		h := new(crypto.Hash.sha256)
 
-		out := hmac_new(key, hkdf.data[i], hashfn)!
+		out := h.hmac(key, hkdf.data[i])!
 		result := out.hex()
 		assert result == sha256_expected_results[i]
 	}
